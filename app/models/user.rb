3c5b5633ae1864fb4c1c0963dbe527c28b0474da
Seanspartacus <iseanburke@gmail.com>
2017-07-05 22:13:33 -0500

    Test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :microposts
 end
+
+class Micropost < ApplicationRecord
+  belongs_to :user
+  validates :content, length: {maximum: 140 }
+  validates :content, presence: { message: "All fields are required."}
+end
