1fd6fe81254a03283d2de653a373bc853300c24a
Seanspartacus <iseanburke@gmail.com>
2017-07-11 15:26:56 -0500

    Require name/email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, length: {maximum: 140 }
   validates :content, presence: { message: "All fields are required."}
+  validates :name, presence: { message: "All fields are required."}
+  validates :email, presence: { message: "All fields are required."}
 end
