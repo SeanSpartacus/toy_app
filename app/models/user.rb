8a32f406f7b7c55bcef914fd33ddd6979e5416eb
Seanspartacus <iseanburke@gmail.com>
2017-07-11 15:30:23 -0500

    Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
 end
 
 class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, length: {maximum: 140 }
   validates :content, presence: { message: "All fields are required."}
-  validates :name, presence: true
-  validates :email, presence: true
 end
